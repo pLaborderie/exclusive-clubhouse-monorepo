3d3541d1f96a25a2778b742098a1524ac5f6a1e3
Paul Laborderie <paul.laborderie@gmail.com>
2021-01-26 11:17:10 +0100

    Handling routes conditions

--- packages/front/src/components/CustomLink.js
+++ packages/front/src/components/CustomLink.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { NavLink, useRouteMatch } from 'react-router-dom';
+import { observer } from 'mobx-react';
+import { UserContext } from '../stores/UserStore';
 
 function CustomLink({ route }) {
+  const { user } = useContext(UserContext);
   const match = useRouteMatch({
     path: route.link,
     exact: route.exact
   });
 
   const linkClass = "inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium" + (match ? "" : " border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700");
+  if (route.condition && !route.condition(user)) {
+    return null;
+  }
   return (
     <NavLink
       to={route.link}
@@ -17,7 +23,7 @@ function CustomLink({ route }) {
     >
       {route.name}
     </NavLink>
-  )
+  );
 }
 
-export default CustomLink;
+export default observer(CustomLink);
--- packages/front/src/router/index.js
+++ packages/front/src/router/index.js
@@ -8,8 +8,8 @@ const routes = [
   { link: '/', name: 'Home', exact: true, component: Home },
   { link: '/signup', name: 'Sign up', condition: (user) => !user, component: Signup },
   { link: '/login', name: 'Log in', condition: (user) => !user, component: Login },
-  { link: '/become-member', name: 'Become a member', condition: (user) => !user.membership, component: Membership },
-  { link: '/become-admin', name: 'Become an admin', condition: (user) => !user.admin, component: Admin },
+  { link: '/become-member', name: 'Become a member', condition: (user) => user && !user.membership, component: Membership },
+  { link: '/become-admin', name: 'Become an admin', condition: (user) => user && !user.admin, component: Admin },
 ];
 
 export default routes;
