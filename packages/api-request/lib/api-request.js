85817a37f94f5e7800d5b43f3feb3321211fcb79
Paul Laborderie <paul.laborderie@gmail.com>
2021-01-27 09:08:25 +0100

    Test console log env

--- packages/api-request/lib/api-request.js
+++ packages/api-request/lib/api-request.js
@@ -3,7 +3,7 @@ import ky from'ky';
 const prefixUrl = process.env.NODE_ENV === 'production'
     ? process.env.PREFIX_URL
     : 'http://localhost:3000';
-
+console.log(process.env);
 const apiRequest = ky.create({
     prefixUrl,
     credentials: 'include'
