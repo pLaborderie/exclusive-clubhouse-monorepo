624a2c4a3ff816502178319e58fcb37503d17583
Paul Laborderie <paul.laborderie@gmail.com>
2021-01-27 09:12:21 +0100

    Abandon env variable

--- packages/api-request/lib/api-request.js
+++ packages/api-request/lib/api-request.js
@@ -1,9 +1,9 @@
 import ky from'ky';
 
 const prefixUrl = process.env.NODE_ENV === 'production'
-    ? process.env.PREFIX_URL
-    : 'http://localhost:3000';
-console.log(process.env);
+    ? 'https://exclusive-clubhouse-api.herokuapp.com/'
+    : 'http://localhost:3000/';
+
 const apiRequest = ky.create({
     prefixUrl,
     credentials: 'include'
